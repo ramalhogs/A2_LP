428ce679a4a180cfd91ed2fe9d44058f945ea027
Breno Azevedo <eborn.azevedo@gmail.com>
2021-11-30 14:56:23 -0300

    Correçao templates

--- polls/urls.py
+++ polls/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('individual-note/', views.individual-note , name='individual-note'),
+    path('individual-note/', views.individualnote , name='individual-note'),
     path('login/', views.login , name='login'),
-    path('note-list/', views.note-list , name='note-list'),
-    path('registro-disciplina/', views.registro-disciplina , name='registro-disciplina')
+    path('note-list/', views.notelist , name='note-list'),
+    path('registro-disciplina/', views.registrodisciplinas, name='registro-disciplina')
 ]
