354037c9e7f6a460f5624d9aca8b0b17a026d7ca
Breno Azevedo <eborn.azevedo@gmail.com>
2021-11-30 14:48:27 -0300

    views e urls atualizado

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-from polls import views
+from polls import views 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('individual-note/', views.individual-note , name='individual-note'),
+    path('login/', views.login , name='login'),
+    path('note-list/', views.note-list , name='note-list'),
+    path('registro-disciplina/', views.registro-disciplina , name='registro-disciplina')
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+# Mudei o nome de algumas funções, pois estavam dando erro. 
+# Os arquivos que contém hífen (-) no nome dão erro. 
+
 def index(request):
-    return HttpResponse("Deu certo.")
+    return render(request, 'index.htm')
+
+def about(request):
+    return render(request, 'about.htm')
+
+def individualnote(request):
+    return render(request, 'individual-note.htm')
+
+def login(request):
+    return render(request, 'login.htm')
+
+def notelist(request):
+    return render(request, 'note-list.htm')
+
+def registrodisciplinas(request):
+    return render(request, 'registro-disciplinas.htm')
